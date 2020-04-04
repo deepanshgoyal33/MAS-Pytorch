2b62d436cc8af54f6d1891c1654bf56293851069
Deepansh Goyal <deepanshgoyal33@gmail.com>
2020-04-04 23:15:10 +0530

    Main Pyhton almost built

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import copy
 import sys 
 
 from utils import *
+from model_utils import *
+
 
 def train():
 
@@ -32,6 +34,60 @@ if __name__="__main__":
     parser.add_argument("--num_freezed",default = 2, type= int, help="No of Layers you want to freeze while extracting features")
     parser.add_argument("--lr", default=.001, type= float, help = 'The initial Learning Rate you want to set')
     parser.add_argument("--lamda", default =.01, type = float, help= "Regularisation Parameter")
+    parser.add_argument("--use_gpu",default= False,type= bool, help="if to use gpu while training or not")
     args= parser.parse_args()
 
+    batch_size = args.batch_size
+    epochs = args.epochs
+    num_frozen = args.num_freezed
+    lr = args.lr
+    scheduler_lambda = args.lamda
 
+    path= os.getcwd()
+
+    train_dataloaders = []
+    test_dataloaders = []
+
+    train_datasets =[]
+    test_datasets=[]
+
+    num_classes=[]
+
+    path= os.getcwd()
+    datapath= os.path.join(path,"data")
+
+    transforms={
+        'train_transforms': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225])
+        ]),
+        'test_transforms': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485,0.456,0.406], [0.229,0.224,0.225])
+        ])
+    }
+    # split the data into the number  tasksyou want to before this step
+    # for our case these are 4
+    # 4 folders in Data folder with no as their task number 
+    for ndir in sorted(os.listdir(data_dir)):
+        
+        ##objects for the train and test image folder
+        train_image_folder = datasets.ImageFolder(os.path.join(datapath,ndir,"train"),transform = transforms['train_transforms'])
+        test_image_folder = datsets.ImageFolder(os.path.join(dataset,dir,'test'),transform = transforms['test_tranforms'])
+
+        ## Loading the data into batches
+        train_dataloader= torch.utils.data.Dataloader(train_image_folder, batch_size= batch_size, shuffle= True,num_workers = 3)
+        test_dataloader = torch.utils.data.Dataloader(test_image_folder, batch_size= batch_size,shuffle= True,num_workers = 1)
+
+        train_size = len(train_image_folder)
+        test_size = len(test_image_folder)
+
+        ##appending the datloader that we have created just now for future use
+        train_dataloaders.append(train_dataloader)
+        test_dataloaders.append(test_dataloader)
+
+        
---
+++ model_utils.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+
+import copy
+import os
+import shutil
+import pickle
+
+from model import *
